00a85f3cad340084e87f6299bebb2ffc69b55aea
koval <artemy.north@gmail.com>
2024-11-09 16:54:22 +0300

    Add protected variables and two functions to get access to them

--- lab_2/headers/InsurancePolicy.h
+++ lab_2/headers/InsurancePolicy.h
@@ -5,16 +5,18 @@
 #include <string.h>
 
 class InsurancePolicy {
-    public:
+    protected:
         std::string holder_name {};
-        std::string policy_number {};
-
+        std::string policy_number {};    
         InsurancePolicy(std::string& name, std::string& number);
 
+    public:
         virtual void displayInfo();
         void displayholderName();
         void changeholderName(std::string new_name);
         void changepolicyNumber(std::string new_number);
+        std::string getholderName();
+        std::string getpolicyNumber();
 
 };
 
